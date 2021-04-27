6a92df0b13b01b1d690be8b03e5ab603f5f7e9cf
Ashish <ashishpatel16.2000@gmail.com>
2021-04-27 20:21:42 +0530

    made minor improvements

--- job_overflow/settings.py
+++ job_overflow/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lw=$)tf_s3(*)8$n28urair%tf1febj*ii7^dd2xm021&u(#mg'
+SECRET_KEY = os.environ.get('JOB_OVERFLOW_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -140,5 +140,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'joboverflowteam@gmail.com'
-EMAIL_HOST_PASSWORD = 'zqrtyaygnbpnsztk'
+EMAIL_HOST_USER = os.environ.get('JOB_OVERFLOW_EMAIL_HOST')
+EMAIL_HOST_PASSWORD = os.environ.get('JOB_OVERFLOW_EMAIL_PASSWORD')
